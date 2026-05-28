d9269061602b78880dc61d9b362f522286f703bc
VulpesNoctis <yakipunal977@gmail.com>
2026-05-28 14:10:30 +0300

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,10 @@
 export default {
     name: "messageCreate",
     async execute(message) {
-      // Botun kendi mesajlarına yanıt vermemesi için kontrol
+     
       if (message.author.bot) return;
 
-      // Mesaj içeriği tam olarak "Yasak" ise yanıt ver
+     
       if (message.content.trim() === "Yasak") {
         message.channel.send("#FreeDiscord");
       }
